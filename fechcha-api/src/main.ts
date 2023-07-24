fbc6d803179fff52a67cdab578a3fdc72aebc8b5
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-07-24 23:00:53 +0100

    doc comments to the main function

--- fechcha-api/src/main.ts
+++ fechcha-api/src/main.ts
@@ -1,7 +1,13 @@
+//this class is entry point to create NestJS applications
 import { NestFactory } from '@nestjs/core';
+
+//root module of the application, which orchestrates the initialization and configuration of other modules in the NestJS application
 import { AppModule } from './app.module';
+
+//ValidationPipe is used for data validation and transformation
 import { ValidationPipe } from '@nestjs/common';
 
+//asynchronous function named bootstrap(), which is used to start the NestJS application
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
