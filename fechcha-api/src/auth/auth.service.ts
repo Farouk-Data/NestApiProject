4af908cd276b5046d3e4568a3643951451a3e836
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-08-02 23:52:31 +0100

    feat: JWT module in Auth

--- fechcha-api/src/auth/auth.service.ts
+++ fechcha-api/src/auth/auth.service.ts
@@ -3,11 +3,14 @@ import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class AuthService {
     //dependency injection
-    constructor(private prisma: PrismaService) {
+    constructor(
+        private prisma: PrismaService,
+        private jwt: JwtService) {
         
     }
     // async funct cs it calls prisma asynchronosly
