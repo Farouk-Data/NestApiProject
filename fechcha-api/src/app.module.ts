96f41903ab884687f64b6306ba67a0fff43ad4da
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-09-07 03:17:04 +0100

    update: update app module

--- fechcha-api/src/app.module.ts
+++ fechcha-api/src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { RankModule } from './rank/rank.module';
-
+import { HomeModule } from './home/home.module';
+import { MatchModule } from './match/match.module';
 
 //config for env
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
-  }), AuthModule, UserModule, PrismaModule, RankModule],
+  }), HomeModule, AuthModule, PrismaModule, RankModule, MatchModule],
 })
 export class AppModule {}
