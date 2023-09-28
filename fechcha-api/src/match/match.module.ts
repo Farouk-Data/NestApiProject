147b44ff762b044f34f5774a055a53f1a20324be
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-09-28 18:54:18 +0100

    imports of rank in match

--- fechcha-api/src/match/match.module.ts
+++ fechcha-api/src/match/match.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { RankModule } from 'src/rank/rank.module';
 import { MatchService } from './match.service';
 import { MatchController } from './match.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
@@ -6,6 +7,6 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 @Module({
   controllers: [MatchController],
   providers: [MatchService],
-  imports: [PrismaModule],
+  imports: [PrismaModule, RankModule],
 })
 export class MatchModule {}
