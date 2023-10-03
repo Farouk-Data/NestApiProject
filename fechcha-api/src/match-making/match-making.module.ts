057648e0dbc31b93ed584f54f0eb262fa1fa79c8
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-10-03 16:46:27 +0100

    fix: add the right imports/exports for matches & matchMaking modules

--- fechcha-api/src/match-making/match-making.module.ts
+++ fechcha-api/src/match-making/match-making.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MatchMakingGateway } from './match-making.gateway';
+import { MatchesModule } from 'src/matches/matches.module';
 
 
 @Module({
-
+  imports: [MatchesModule],
   providers: [MatchMakingGateway]
 })
 export class MatchMakingModule {}
--- fechcha-api/src/matches/matches.module.ts
+++ fechcha-api/src/matches/matches.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MatchesService } from './matches.service';
 import { MatchesGateway } from './matches.gateway';
+import { MatchesController } from './matches.controller';
 
 @Module({
-  providers: [MatchesGateway, MatchesService]
+  controllers: [MatchesController],
+  providers: [MatchesGateway, MatchesService],
+  exports: [MatchesService],
 })
 export class MatchesModule {}
