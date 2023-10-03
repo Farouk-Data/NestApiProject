728df24ac32021874a1f320f13a5ee5688a5deab
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-10-03 16:43:27 +0100

    chore: delete DTOs

--- fechcha-api/src/match-making/dto/update-match-making.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMatchMakingDto } from './create-match-making.dto';
-
-export class UpdateMatchMakingDto extends PartialType(CreateMatchMakingDto) {
-  id: number;
-}
