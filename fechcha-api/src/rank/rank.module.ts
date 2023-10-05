7b5833004398fb0c7da4c27d180ab7b5d72bd2b5
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-10-06 00:02:45 +0100

    feat: add prismaModule & matchesModule to rank module for dependencies

--- fechcha-api/src/rank/rank.module.ts
+++ fechcha-api/src/rank/rank.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { RankService } from './rank.service';
 import { RankController } from './rank.controller';
 import { AuthModule } from 'src/auth/auth.module';
+import { MatchesModule } from 'src/matches/matches.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, MatchesModule, PrismaModule, ],
   controllers: [RankController],
   providers: [RankService],
   exports : [RankService],
